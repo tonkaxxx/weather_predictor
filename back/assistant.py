2892c3be147a4bf62093d3402dc3d3f2f00ccb76
tonkaxxx <maltsev.yar@gmail.com>
2025-11-13 13:36:09 +0300

    ai assistant api

--- back/assistant.py
+++ back/assistant.py
@@ -1,6 +1,10 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import requests
+from flask import Flask, request, render_template, jsonify
 
+app = Flask(__name__)
+
+url = "http://192.168.88.17:5000/api/get-24hrs"
 model_name = "Qwen/Qwen3-4B-Instruct-2507"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(
@@ -9,48 +13,74 @@ model = AutoModelForCausalLM.from_pretrained(
     device_map="auto"
 )
 
-url = "http://localhost:5000/api/get-24hrs"
-response = requests.post(url, json={"city": "Москва"})
+def ask_ai(user_input, city):
+    response = requests.post(url, json={"city": city})
 
-print(response.json())
+    # нужно сделать обработку неудачи в апи
+    weather_data = response.json()['for_ai']
+    print(weather_data)
 
-weather_data = response.json()
+    # print("Погодный ассистент запущен. Введите ваш вопрос:")
+    # user_input = input("\nВы: ")
 
-print("Погодный ассистент запущен. Введите ваш вопрос:")
+    prompt = f"""
+    Ты - ассистент погоды. Отвечай ТОЛЬКО на основе данных о погоде. Не использу язык разметки markdown.
+    Если данных нет - скажи "В прогнозе нет этой информации".
 
-user_input = input("\nВы: ")
+    Данные о погоде на сегодня: {weather_data}
+    Если влажность больше 85 процентов - говори что идет дождь. Но не проговаривай условие.
 
-prompt = f"""
-Ты - ассистент погоды. Отвечай ТОЛЬКО на основе данных о погоде.
-Если данных нет - скажи "В прогнозе нет этой информации".
+    Вопрос: {user_input}
 
-Данные о погоде на сегодня: {weather_data}
+    Ответ:"""
 
-Вопрос: {user_input}
+    messages = [{"role": "user", "content": prompt}]
+    text = tokenizer.apply_chat_template(
+        messages,
+        tokenize=False,
+        add_generation_prompt=True,
+        # enable_thinking=True
+    )
 
-Ответ:"""
+    model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
+    generated_ids = model.generate(
+        **model_inputs,
+        max_new_tokens=512
+    )
 
-messages = [{"role": "user", "content": prompt}]
-text = tokenizer.apply_chat_template(
-    messages,
-    tokenize=False,
-    add_generation_prompt=True,
-    # enable_thinking=True
-)
+    output_ids = generated_ids[0][len(model_inputs.input_ids[0]):].tolist()
 
-model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
-generated_ids = model.generate(
-    **model_inputs,
-    max_new_tokens=512
-)
+    try:
+        index = len(output_ids) - output_ids[::-1].index(151668)
+        thinking_content = tokenizer.decode(output_ids[:index], skip_special_tokens=True)
+        content = tokenizer.decode(output_ids[index:], skip_special_tokens=True)
+    except:
+        content = tokenizer.decode(output_ids, skip_special_tokens=True)
 
-output_ids = generated_ids[0][len(model_inputs.input_ids[0]):].tolist()
+    return content.strip()
 
-try:
-    index = len(output_ids) - output_ids[::-1].index(151668)
-    thinking_content = tokenizer.decode(output_ids[:index], skip_special_tokens=True)
-    content = tokenizer.decode(output_ids[index:], skip_special_tokens=True)
-except:
-    content = tokenizer.decode(output_ids, skip_special_tokens=True)
+@app.route('/api/ask-ai', methods=['POST'])
+def api_ask_ai():
+    try:
+        data = request.get_json()
+        city = data.get('city', '')
+        user_input = data.get('user_input', '')
 
-print(f"Ассистент: {content.strip()}")
+        response = ask_ai(user_input, city)
+        
+        return jsonify({
+            'status': 'success',
+            'response': response
+        }), 200
+    
+    except Exception as e:
+        return jsonify({
+            'status': 'error',
+            'message': str(e)
+        }), 500
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5050, debug=False)
+
+    print(ask_ai("какая погода на сегодня?", "Москва"))
